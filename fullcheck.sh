d0cd4cc9964e0807745bace37faed810dc67ba0b
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-12 10:57:59 +0400

    Added style checking scripts
    Change-Id: Ic8dddad86a4513772f7492813cd607f862dd7045

---
+++ fullcheck.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find -name "*.cc" -not -path "./tests/google/*" -not -path "./src/boost/*" -exec python cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators {} \;
+find -name "*.h"  -not -name "config.h" -not -path "./inc/boost/*" -not -path "./tests/google/*" -exec python cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators {} \;
